026835c1d83d15cb982b24808785493a9af9b24f
papeixoto <pazevedopeixoto@gmail.com>
2025-07-28 11:48:14 +0100

    js basis exercises

---
+++ 0.3-getting-started-with-js-v2/0-primer/primer.js
@@ -0,0 +1,23 @@
+function addFavoriteBook(bookName) {
+  if (!bookName.includes("Great")) {
+    favoriteBooks.push(bookName);
+  }
+}
+
+function printFavoriteBooks() {
+  console.log(`Favorite Books: ${favoriteBooks.length}`);
+  for (let i = 0; i < favoriteBooks.length; i++) {
+    console.log(favoriteBooks[i]);
+  }
+}
+
+var favoriteBooks = [];
+
+addFavoriteBook("A Song of Ice and Fire");
+addFavoriteBook("The Great Gatsby");
+addFavoriteBook("Crime & Punishment");
+addFavoriteBook("Great Expectations");
+addFavoriteBook("You Don't Know JS");
+
+// TODO: print out favorite books
+printFavoriteBooks();
